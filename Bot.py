cb4285f3ecbbf118dcaea439d22a45771606df00
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 17:24:11 -0400

    Fixed Multiple instances and no messages showing

--- Bot.py
+++ Bot.py
@@ -4,15 +4,14 @@ from discord.ext import commands #gets commands for discord bot
 import logging #for logging information to cmd
 from pathlib import Path #for paths
 import json
-
+import os
 
 token = ""
 with open("token.txt", "r") as f:
-	token = f.readline()
+	token = f.readline().strip()
 
 
-
-client = discord.Client()#creates and instance of the client that connects to discord
+client = discord.Client(intents = discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -20,7 +19,12 @@ async def on_ready():
 	
 @client.event
 async def on_message(message):
-	if message.content.startswith("!send") != -1:
-		await message.channel.send("Test")
+	if message.content.find("!hello") != -1:
+		await message.channel.send("Hi")
+	
+	if message.content.find("!end") != -1:
+		exit()
 
+print(token)
 client.run(token)
+
