9fb04a51e7b31220c6edf69ee6779c508a491da8
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 16:03:44 -0400

    Fixed token issue

--- Bot.py
+++ Bot.py
@@ -3,14 +3,16 @@ import discord #for discord
 from discord.ext import commands #gets commands for discord bot
 import logging #for logging information to cmd
 from pathlib import Path #for paths
+import json
+
+
+token = ""
+with open("token.txt", "r") as f:
+	token = f.readline()
 
 
 
-
-token = "MTA4NDY1MjY3OTMyMzcxMzU4Nw.Gww3iS.W_Fu1O6zggE-zvCuGTyh3zH3x_SQQ5Zbqr3dsw"
 client = discord.Client()#creates and instance of the client that connects to discord
-
-
 
 @client.event
 async def on_ready():
@@ -18,9 +20,7 @@ async def on_ready():
 	
 @client.event
 async def on_message(message):
-	id = client.get_guild(1084655332703350844)
 	if message.content.startswith("!send") != -1:
 		await message.channel.send("Test")
 
 client.run(token)
-	
